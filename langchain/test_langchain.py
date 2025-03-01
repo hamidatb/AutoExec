260dfaa5a49afe6de394ef2e2c559ef2c5223df6
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2025-03-01 14:41:31 -0700

    setup: create langchain tester file

--- langchain/test_langchain.py
+++ langchain/test_langchain.py
@@ -1,6 +1,23 @@
 import getpass
 import os
 import time
-
 import langchain
+
+from dotenv import load_dotenv
+
 from pinecone import Pinecone, ServerlessSpec
+from langchain_openai import ChatOpenAI # type: ignore
+
+# get the Pinecone and openAI api kets
+if not load_dotenv():
+    raise EnvironmentError("Failed to load .env file")
+    
+llm = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
+llm("Explain Hamidat Bello to me")
